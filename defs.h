e073d8f3e9f400a6ad0f77b4880bc320f7aefa91
Toni Spets <toni.spets@iki.fi>
2014-06-07 19:42:25 +0300

    Bindings menu is gone

--- defs.h
+++ defs.h
@@ -1059,9 +1059,9 @@
 // unused             				6
 // unused               			7
 #define MENU_REPEATHELP 			8
-#define MENU_PICKBINDS  			9
-#define MENU_SHOWBINDS1 			10
-#define MENU_SHOWBINDS2 			11
+// unused                                       9
+// unused                                       10
+// unused                                       11
 #define MENU_SPY				12
 #define MENU_SPY_SKIN				13
 #define MENU_SPY_COLOR				14
