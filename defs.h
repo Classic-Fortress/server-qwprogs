1b74ab13073c2830ed546e5213fc78989c622c39
Niclas Lindstedt <mpezar@gmail.com>
2014-06-10 22:12:13 +0200

    Autozoom: code cleanup, fix stuff flooding, add support for non-standard default fovs, implement smooth zooming

--- defs.h
+++ defs.h
@@ -225,13 +225,11 @@
 #define TFSTATE_BURNING			512 // Is on fire
 #define TFSTATE_GRENTHROWING		1024  // is throwing a grenade
 #define TFSTATE_AIMING			2048  // is using the laser sight
-#define TFSTATE_ZOOMOFF         	4096  // doesn't want the FOV changed when zooming
+//unused                     	4096
 #define TFSTATE_RESPAWN_READY		8192  // is waiting for respawn, and has pressed fire
 #define TFSTATE_HALLUCINATING		16384  // set when player is hallucinating
 #define TFSTATE_TRANQUILISED 		32768  // set when player is tranquilised
 #define TFSTATE_CANT_MOVE		65536  // set when player is setting a detpack
-#define TFSTATE_ZOOM4X       131072  // set 2x autozoom
-#define TFSTATE_ZOOM2X       262144  // set 4x autozoom
 
 // Defines used by TF_T_Damage (see combat.qc)
 #define TF_TD_IGNOREARMOUR	1  // Bypasses the armour of the target
