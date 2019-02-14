4c062f4f274c7e54d2af8129dc85bf987e9a3afc
fdittz <fdittz@gmail.com>
2019-02-14 17:18:50 -0200

    player ready and startmatch

--- defs.h
+++ defs.h
@@ -457,9 +457,9 @@
 #define TF_VOTETRICK            132 // Vote to start voting for a trick map
 #define TF_VOTERACE             133 // Vote to start voting for a race map
 #define TF_FORCENEXT            134 // Vote to force a change to voted map
-// unused                       135
-// unused                       136
-// unused                       137
+#define TF_PLAYERREADY          135
+#define TF_PLAYERBREAK          136
+#define TF_FORCESTARTMATCH       137
 // unused                       138
 // unused                       139
 // unused                       140
