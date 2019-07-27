9a61846732677442e3624d769d0b1d31956c3577
danni <danni.allen@gmail.com>
2019-07-27 22:19:25 +1000

    make defs shared, add some hud defines

--- share/defs.h
+++ share/defs.h
@@ -1291,3 +1291,14 @@
 #define RESPAWN_DELAY_TIME	5	// this is the respawn delay, if the RESPAWN_DELAY option is
 					// turned on with temp1.  QuakeWorld servers can use
 					// serverinfo respawn_delay to set their own time.
+
+
+// CSQC defines
+#define FO_HUD_GROW_UP		1
+#define FO_HUD_GROW_DOWN	2
+#define FO_HUD_GROW_LEFT	3
+#define FO_HUD_GROW_RIGHT	4
+
+#define FO_HUD_INSERT_AFTER		1
+#define FO_HUD_INSERT_BEFORE	1
+
