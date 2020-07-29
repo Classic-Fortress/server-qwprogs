902109ebfbdc3de80cddeb62f0985ea85bd3e43b
danni <danni.allen@gmail.com>
2020-07-29 10:49:34 +1000

    halfway implemented

--- share/defs.h
+++ share/defs.h
@@ -1355,6 +1355,8 @@
 #define FO_HUD_MENU_HINT_NAME "Menu Hints"
 #define FO_HUD_GAME_MODE_NAME "Game Mode"
 #define FO_HUD_READY_NAME "Ready Status"
+#define FO_HUD_SHOWSCORES_NAME "Show Scores"
+
 
 #define ICON_CLIPSIZE "textures/wad/clipsize.png"
 #define ICON_FRAGSTREAK "textures/wad/fragstreak.png"
