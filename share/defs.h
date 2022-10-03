d9aca9bf938cb029350786e0d14001d5ae60444b
newby <newby@rakis.net>
2022-10-02 17:14:52 -0700

    Harden interfaces with opaque Slot tokens
    We've seen one or two crashes with out of bound slots, the code is also
    confusing in terms of trying to determine when you need to do input checking and
    when you can just trust that a slot is valid.  Try to better formalize this by
    introducing an opaque slot token type that references valid slots.

--- share/defs.h
+++ share/defs.h
@@ -297,6 +297,9 @@
 /* Toggleable Game Settings				*/
 /*======================================================*/
 
+// Opaque token that encapsulates slots for correctness.
+struct Slot { int id; };
+
 // Some of the toggleflags aren't used anymore, but the bits are still
 // there to provide compatability with old maps
 #define TFLAG_CLASS_PERSIST	1  	// Persistent Classes Bit
@@ -372,14 +375,11 @@
 /*======================================================*/
 /* Impulse Defines                                      */
 /*======================================================*/
-#define TF_SLOT_NONE                0
-#define TF_SLOT1                    1   // Changes weapon to slot 1 (primary weapon)
-#define TF_SLOT2                    2   // Changes weapon to slot 2 (secondary weapon)
-#define TF_SLOT3                    3   // Changes weapon to slot 3 (tertiary weapon)
-#define TF_SLOT4                    4   // Changes weapon to slot 4 (melee weapon)
-#define TF_SLOT_LAST                TF_SLOT4
-#define TF_SLOT_MELEE               TF_SLOT4
-#define TF_NUM_SLOTS                (TF_SLOT_LAST - TF_SLOT1 + 1)
+#define TF_IMPULSE_SLOT1            1   // Changes weapon to slot 1 (primary weapon)
+#define TF_IMPULSE_SLOT2            2   // Changes weapon to slot 2 (secondary weapon)
+#define TF_IMPULSE_SLOT3            3   // Changes weapon to slot 3 (tertiary weapon)
+#define TF_IMPULSE_SLOT4            4   // Changes weapon to slot 4 (melee weapon)
+#define TF_NUM_SLOTS                4
 
 #define TF_CLASSMENU                5   // Brings up class menu
 // unused                           6
